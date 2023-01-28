522f391bff0bbdf5fadba2b36a21cf06e230e76f
ecalisk <emirhane95@gmail.com>
2023-01-27 19:21:15 -0500

    Complete exercise 04-removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...toBeRemoved) {
+  for (let i = 0; i < toBeRemoved.length; i++) {
+    for (let k = 0; k < array.length; k++) {
+      if (array[k] == toBeRemoved[i]) {
+        if (typeof array[k] == typeof toBeRemoved[i]) array.splice(k, 1);
+      }
+    }
+  }
+  return array;
 };
 
 // Do not edit below this line
