ee2c91af0c0198adc5fdafe3c5045f6e19b4e39f
ecalisk <emirhane95@gmail.com>
2023-01-27 20:04:04 -0500

    Complete exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  // larger number first
+  if (num1 > num2) {
+    let buffer = num1;
+    num1 = num2;
+    num2 = buffer;
+  }
 
+  // error for negative numbers
+  if (num1 < 0 || num2 < 0) return "ERROR";
+
+  // error for non-number input
+  if (typeof num1 != "number" || typeof num2 != "number") return "ERROR";
+
+  let i = num1;
+  let outcome = 0;
+  while (i <= num2) {
+    outcome += i;
+    i++;
+  }
+  return outcome;
 };
 
 // Do not edit below this line
