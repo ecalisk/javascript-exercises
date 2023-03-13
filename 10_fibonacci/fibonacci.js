febf782310722d95d39d33c4b05a4394782b29fd
ecalisk <emirhane95@gmail.com>
2023-03-12 23:06:40 -0400

    Complete exercise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function (number) {
+  if (number < 0) return "OOPS";
+  if (typeof number === "string") number = Number(number);
+  let sequence = [1, 1];
+  for (i = 2; i <= number - 1; i++) {
+    sequence[i] = sequence[i - 1] + sequence[i - 2];
+  }
+  return sequence[sequence.length - 1];
 };
 
 // Do not edit below this line
