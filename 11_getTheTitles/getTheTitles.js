43c196b28628fe86d2fa098c77d76158d037846d
ecalisk <emirhane95@gmail.com>
2023-03-13 18:34:51 -0400

    Complete exercise 11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (books) {
+  const titleArray = books.map((book) => book.title);
+  return titleArray;
 };
 
 // Do not edit below this line
