8af345e567ed30a6c7ffa4f9f017ffaa9f2df9d5
ecalisk <emirhane95@gmail.com>
2023-03-12 22:38:03 -0400

    Complete exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,11 @@
-const palindromes = function () {
+const palindromes = function (stringInput) {
+  let myArray = stringInput
+    .toLowerCase()
+    .split("")
+    .filter((char) => char !== " ")
+    .filter((char) => char.charCodeAt(0) >= 97 && char.charCodeAt(0) <= 122);
 
+  return myArray.join("") === myArray.reverse().join("");
 };
-
 // Do not edit below this line
 module.exports = palindromes;
