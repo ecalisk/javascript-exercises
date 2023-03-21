9b5ffffba8bfcab9609d682c76ef87ecef856973
ecalisk <emirhane95@gmail.com>
2023-03-21 12:55:26 -0400

    Complete exercise 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  const peopleSorted = people.sort(function (personA, personB) {
+    const currentYear = new Date().getFullYear();
+    const ageA =
+      typeof personA.yearOfDeath === "undefined"
+        ? currentYear - personA.yearOfBirth
+        : personA.yearOfDeath - personA.yearOfBirth;
 
+    const ageB =
+      typeof personB.yearOfDeath === "undefined"
+        ? currentYear - personB.yearOfBirth
+        : personB.yearOfDeath - personB.yearOfBirth;
+
+    return ageB - ageA;
+  });
+  return peopleSorted[0];
 };
 
 // Do not edit below this line
