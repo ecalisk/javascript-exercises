c57cf20ad6d35b7ba1a5e371d2c190225d29e7ef
ecalisk <emirhane95@gmail.com>
2023-04-08 15:00:44 -0400

    Partially complete exercise 13_caesar

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,28 @@
-const caesar = function() {
+const caesar = function (originalMessage, shiftNumber) {
+  const messageArray = originalMessage.split("");
+  let encryptedArray = [];
+  for (let i = 0; i < messageArray.length; i++) {
+    code = messageArray[i].charCodeAt(0);
+    let codeAdjusted = code;
+    let shiftedCode = code + shiftNumber;
+    if (97 <= code && code <= 122) {
+      if (shiftedCode > 122) codeAdjusted = code - 26;
+      else if (shiftedCode < 97) codeAdjusted = code + 26;
+    } else if (65 <= code && code <= 90) {
+      if (shiftedCode > 90) codeAdjusted = code - 26;
+      else if (shiftedCode < 65) codeAdjusted = code + 26;
+    }
 
+    suitableForShift =
+      (65 <= code && code <= 90) || (97 <= code && code <= 122);
+
+    if (suitableForShift) {
+      encryptedArray[i] = String.fromCharCode(codeAdjusted + shiftNumber);
+    } else {
+      encryptedArray[i] = String.fromCharCode(codeAdjusted);
+    }
+  }
+  return encryptedArray.join("");
 };
 
 // Do not edit below this line
